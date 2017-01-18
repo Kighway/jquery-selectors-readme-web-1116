fbab3b352d6043b2a3ed7202fc516fae5225c738
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-01-18 14:57:53 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+// $(document).ready(function() {
+//   alert("Hi")
+// });
+
+function paragraphSelector () {
+  return $('p')
+}
+
+function lastImageSelector () {
+  return $('img').last()
+}
+
+function ninjaBabySelector () {
+  return $('#baby-ninja')
+}
+
+function divSelector () {
+  return $('.pics')
+}
+
+function firstListItem () {
+  // $('div.pics ul#pic-list').first()
+  return $('div.pics ul li').first()
+}
